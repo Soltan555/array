8dc4895f79d892adcd62088e56b686530a8e2a9d
Soltan555 <98511484+Soltan555@users.noreply.github.com>
2022-03-26 10:37:25 +0400

    Add files via upload

---
+++ 15/script.js
@@ -0,0 +1,46 @@
+// let car = [
+//     "BMW",
+//     'Mercedes',
+//     'Hundai',
+//     'Infiniti',
+//     'Hummer',
+//     'Toyota',
+//     'Opel',
+//     'Nissan',
+//     'Porsche',
+//     'Wolkswagen',
+//     'Lexus',
+//     'Range Rover',
+//     'Ford',
+//     'Audi'
+// ];
+
+// let cars = document.getElementById("car");
+// let carsimg = document.getElementById("carsimg").src
+
+
+
+
+// onload = function() {
+//     let data = '<option  selected disabled> Masin secin </option>';
+//     for (let item in car) {
+//         data += `<option value="${i}">${car[item]}</option>`
+//     }
+//     cars.innerHTML = data;
+// }
+
+
+// onload = function() {
+//     let data = '<option  selected disabled> Masin secin </option>';
+//     for (let item in car) {
+//         data += `<option value="${item}">${car[item]}</option>`
+//     }
+//     cars.innerHTML = data;
+// }
+
+
+let select = document.getElementById("car"),
+    arr = ["BMW", "Mercedes", "Hundai", "Infiniti", "Hummer", "Toyota", "Opel", "Nissan", "Porsche", "Wolkswagen", "Lexus", "Range Rover", "Ford", "Audi"],
+
+    for (let i = 0 < arr.length;
+        "i++")
